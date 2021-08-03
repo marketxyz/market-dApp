6afed94dbac6971ee501b8bfb8f4f1d330ddf4b0
Digvijay Singh Rathore <dynamicdigvijay@gmail.com>
2021-08-03 17:16:02 +0530

    fuse navbar w/ funcctionality

--- src/components/pages/Fuse/FuseDashNav.tsx
+++ src/components/pages/Fuse/FuseDashNav.tsx
@@ -17,11 +17,23 @@ import {
   InputGroup,
   InputLeftAddon,
   Input,
+  Center,
 } from "@chakra-ui/react";
-import { PixelSize, Row } from "utils/chakraUtils";
+import { PixelSize, Row, useWindowSize } from "utils/chakraUtils";
 
 import { useTranslation } from "react-i18next";
 import { PhoneIcon, SearchIcon } from "@chakra-ui/icons";
+import DashboardBox from "components/shared/DashboardBox";
+import { useIsSmallScreen } from "hooks/useIsSmallScreen";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useFilter } from "./FuseTabBar";
+
+import { Link as RouterLink } from "react-router-dom";
+
+const activeStyle = { bg: "#FFF", color: "#000" };
+const noop = {};
+const selectedTabStyles = {borderColor: "#DF2EAC", fontSize: "18px"}
+const tabStyles = {paddingBottom: "20px", fontSize: "18px"}
 
 export const FuseDashNav = ({
   isAuthed,
@@ -36,17 +48,24 @@ export const FuseDashNav = ({
 }) => {
   const { t } = useTranslation();
 
+  let { poolId } = useParams();
+
+  let navigate = useNavigate();
+
+  const filter = useFilter();
+  const location = useLocation();
+
   return (
     <Row
       color="#000000"
-      paddingTop="20px"
+      paddingTop="15px"
       mainAxisAlignment="space-between"
       crossAxisAlignment="center"
       overflowX="visible"
       overflowY="visible"
       width="100%"
-      borderBottom="1px solid #D8D8D8"
-      borderTop="1px solid #D8D8D8"
+      borderBottom="1px solid #e6e4e7"
+      borderTop="1px solid #e6e4e7"
       backgroundColor="#FFFFFF"
     >
 
@@ -59,33 +78,96 @@ export const FuseDashNav = ({
             width="100%"
         >
 
-            <Tabs>
-                <TabList>
-                    <Tab style={tabStyles} _selected={selectedTabStyles}>Your Pools</Tab>
-                    <Tab style={tabStyles} _selected={selectedTabStyles}>All Pools</Tab>
-                    <Tab style={tabStyles} _selected={selectedTabStyles}>Token</Tab>
-                </TabList>
-            </Tabs>
+          <Tabs defaultIndex={1}>
+              <TabList>
+                  <Tab _selected={selectedTabStyles}>
+                    <TabLink route="/fuse?filter=my-pools" text={t("Your Pools")} />
+                  </Tab>
+                  <Tab  _selected={selectedTabStyles}>
+                    <TabLink route="/fuse" text={t("All Pools")} />
+                  </Tab>
+                  <Tab _selected={selectedTabStyles}>
+                    <TabLink route="/fuse" text={t("Token")} />
+                  </Tab>
+              </TabList>
+          </Tabs>
 
-            <InputGroup paddingBottom="15px" width="35%">
-                <InputLeftAddon
-                    pointerEvents="none"
-                    children={<SearchIcon color="#62526A" />}
-                    backgroundColor="#FFFFFF"
-                    borderRight="none"
-                />
-                <Input type="tel" placeholder="Search..." />
-            </InputGroup>
+          <InputGroup paddingBottom="15px" width="35%">
+              <InputLeftAddon
+                  pointerEvents="none"
+                  children={<SearchIcon color="#62526A" />}
+                  backgroundColor="#FFFFFF"
+                  border="2.5px solid #d9d8da"
+              />
+              <Input
+                _focus={{outline: "none"}}
+                _hover={{}}
+                border="2.5px solid #d9d8da"
+                fontSize="18px"
+                borderLeft="none"
+                borderRadius="11px"
+                type="text"
+                value={filter ?? ""}
+                onChange={(event) => {
+                  const value = encodeURIComponent(event.target.value);
+
+                  if (value) {
+                    navigate("?filter=" + value);
+                  } else {
+                    navigate("");
+                  }
+                }}
+                placeholder="Try searching for USDC"
+              />
+          </InputGroup>
 
         </Row>
     </Row>
   );
 };
 
-const selectedTabStyles = {
-    borderColor: "pink"
-}
+// copied code
 
-const tabStyles = {
-    paddingBottom: "20px"
-}
+const TabLink = ({ route, text }: { route: string; text: string }) => {
+  const isMobile = useIsSmallScreen();
+
+  const location = useLocation();
+
+  return (
+    <Link
+      /* @ts-ignore */
+      style={tabStyles}
+      as={RouterLink}
+      className="no-underline"
+      to={route}
+      ml={isMobile ? 0 : 4}
+      mt={isMobile ? 4 : 0}
+      {...(route ===
+      location.pathname.replace(/\/+$/, "") + window.location.search
+        ? activeStyle
+        : noop)}
+    >
+        {text}
+    </Link>
+  );
+};
+
+const TabExternalLink = ({ route, text }: { route: string; text: string }) => {
+  const isMobile = useIsSmallScreen();
+
+  return (
+    <Link
+      className="no-underline"
+      href={route}
+      isExternal
+      ml={isMobile ? 0 : 4}
+      mt={isMobile ? 4 : 0}
+    >
+      <DashboardBox height="35px">
+        <Center expand px={2} fontWeight="bold">
+          {text}
+        </Center>
+      </DashboardBox>
+    </Link>
+  );
+};
