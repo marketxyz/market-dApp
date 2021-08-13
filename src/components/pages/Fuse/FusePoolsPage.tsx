e9cdd382e3a23c7fc4a5aa97ad3c726f34e362e1
Digvijay Singh Rathore <dynamicdigvijay@gmail.com>
2021-08-14 00:30:00 +0530

    pagination for fuse pools page

--- src/components/pages/Fuse/FusePoolsPage.tsx
+++ src/components/pages/Fuse/FusePoolsPage.tsx
@@ -1,9 +1,9 @@
-import { SimpleGrid as Grid, Spinner, Center } from "@chakra-ui/react";
+import { SimpleGrid as Grid, Spinner, Center, Text, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Box } from "@chakra-ui/react";
 import PageTransitionLayout from "components/shared/PageTransitionLayout";
 import { useRari } from "context/RariContext";
 import { useFusePools } from "hooks/fuse/useFusePools";
 import { useIsSmallScreen } from "hooks/useIsSmallScreen";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { FuseDashNav } from "./FuseDashNav";
 import FusePageLayout from "./FusePageLayout";
 import PoolCard from "./FusePoolCard";
@@ -26,29 +26,93 @@ export default FusePoolsPage;
 
 const PoolList = () => {
   const filter = useFilter();
-  const { filteredPools } = useFusePools(filter);
+  const { filteredPools }: any = useFusePools(filter);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const poolsPerPage = 5;
+  const indexOfLastPool = currentPage * poolsPerPage;
+  const indexOfFirstPool = indexOfLastPool - poolsPerPage;
+  const currentPools = filteredPools?.slice(indexOfFirstPool, indexOfLastPool);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return filteredPools ? (
-    <Grid
-      templateRows={{
-        base: "repeat(1, minmax(0, 1fr))",
-        lg: "repeat(2, minmax(0, 1fr))",
-      }}
-      autoFlow="row"
-      columns={{ base: 1, lg: 2 }}
-      my="2rem"
-      w={{ base: "90%", lg: "100%" }}
-      maxW={{ lg: "1200px" }}
-      mx="auto"
-      gridGap="8"
-    >
-      {filteredPools.map((pool, index) => {
-        return <PoolCard data={pool} />;
-      })}
-    </Grid>
+    <>
+      <Grid
+        templateRows={{
+          base: "repeat(1, minmax(0, 1fr))",
+          lg: "repeat(2, minmax(0, 1fr))",
+        }}
+        autoFlow="row"
+        columns={{ base: 1, lg: 2 }}
+        my="2rem"
+        w={{ base: "90%", lg: "100%" }}
+        maxW={{ lg: "1200px" }}
+        mx="auto"
+        gridGap="8"
+      >
+        {currentPools.map((pool: any, index: any) => {
+          return <PoolCard data={pool} />;
+        })}
+      </Grid>
+      <Box
+        maxW={{ lg: "1200px" }}
+        w="100%"
+        mx="auto"
+        mb="10"
+        textAlign="center"
+      >
+        <Pagination currentPage={currentPage} poolsPerPage={poolsPerPage} totalPools={filteredPools.length} paginate={paginate} />
+      </Box>
+    </>
   ) : (
     <Center width="100%">
       <Spinner my={40} />
     </Center>
   );
 };
+
+const Pagination = ({totalPools, poolsPerPage, paginate, currentPage}: any) => {
+  let pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPools / poolsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <Box
+      py="4"
+      width="100%"
+    >
+      {pageNumbers.map((num: any) => (
+        currentPage === num ? (
+          <Text
+            fontSize="lg"
+            bgGradient="linear(to-r, #BB3EF0, #5109B0)"
+            color="#FFF"
+            display="inline"
+            px="4"
+            py="2"
+            borderRadius="5px"
+            onClick={() => paginate(num)}
+            cursor="pointer"
+          >{num}</Text>
+        ) : (
+          <Text
+            fontSize="lg"
+            bg="white"
+            _hover={{bg: "gray.100"}}
+            color="#606060"
+            display="inline"
+            px="4"
+            py="2"
+            mx="2"
+            borderRadius="5px"
+            onClick={() => paginate(num)}
+            cursor="pointer"
+          >{num}</Text>
+        )
+      ))}
+    </Box>
+  )
+}
