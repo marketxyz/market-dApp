fd4dd19ed91f656397e31161b82fee005523bed0
Aayush Pal <ap.aayushpal@gmail.com>
2021-10-08 12:42:32 +0530

    Update getPoolsByOwner api code: Create & send json response

--- api/getPoolsByOwner.ts
+++ api/getPoolsByOwner.ts
@@ -6,21 +6,45 @@ import {
 
 const fuse = initFuseWithProviders(turboGethURL);
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: VercelRequest, res: VercelResponse) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  let pools = await fuse.contracts.FusePoolLens.methods
+
+  let poolData = await fuse.contracts.FusePoolLens.methods
     .getPublicPoolsWithData()
     .call({ gas: 1e18 });
 
   let owner = req.query.owner;
-  let matchedPool = {};
-  pools[1].map((p) => {
-    if (p.comptroller === owner) {
-      matchedPool = p;
+  let matchedPool: Record<any, any>[] = [];
+  for (let [idx, p] of Object.entries(poolData[1]) as any) {
+    const contract = new fuse.web3.eth.Contract(
+      JSON.parse(
+        fuse.compoundContracts["contracts/Comptroller.sol:Comptroller"].abi
+      ),
+      p.comptroller
+    );
+
+    try {
+      let admin = await contract.methods.admin().call({ gas: 1e18 });
+      if (admin === owner) {
+        matchedPool.push({
+          name: p.name,
+          creator: p.creator,
+          comptroller: p.comptroller,
+          blockPosted: p.blockPosted,
+          timestampPosted: p.timestampPosted,
+          supply: poolData[2][idx],
+          borrow: poolData[3][idx],
+          underlyingTokens: poolData[4][idx],
+          underlyingSymbols: poolData[5][idx]
+        });
+      }
+    } catch (e) {
+      // ignore
     }
-  });
+  }
 
   res.setHeader("Cache-Control", "maxage=600, s-maxage=600");
-  
+
   return res.json(matchedPool);
 };
