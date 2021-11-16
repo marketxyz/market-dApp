3a41c1cb7d72aa49fb80b13a8522c940de148353
Aayush Pal <ap.aayushpal@gmail.com>
2021-11-16 18:15:39 +0530

    feature: initial commit

--- api/assetVariation.ts
+++ api/assetVariation.ts
@@ -1,13 +1,20 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import fetch from "node-fetch";
+import { variance } from "mathjs";
 
-// note: this acts like a proxy for beefy APY and caches it for 10mins
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (request: VercelRequest, response: VercelResponse) => {
+  const {address, poolId} = <Record<string, string>>request.query;
+  
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Cache-Control", "max-age=600, s-maxage=600");
 
-  const data = await fetch("https://api.beefy.finance/apy");
+  const data = await fetch(
+    `https://api.coingecko.com/api/v3/coins/ethereum/contract/${address}/market_chart/?vs_currency=usd&days=30`
+  )
+    .then((res) => res.json())
+    .then((data) => data.prices.map(([, price]) => price))
+    .then((prices) => variance(prices));
+
   const json = await data.json();
 
   return response.json(json);
--- api/defiCoins.ts
+++ api/defiCoins.ts
@@ -1,14 +1,16 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import fetch from "node-fetch";
 
-// note: this acts like a proxy for beefy APY and caches it for 10mins
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (request: VercelRequest, response: VercelResponse) => {
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Cache-Control", "max-age=600, s-maxage=600");
 
-  const data = await fetch("https://api.beefy.finance/apy");
-  const json = await data.json();
+  const data = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=decentralized_finance_defi&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+  )
+    .then((res) => res.json())
+    .then((array) => array.slice(0, 30));
 
+  const json = await data.json();
   return response.json(json);
 };
