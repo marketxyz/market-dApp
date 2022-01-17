3a354ecc7cd8c2245b8e1ea58f8d11fba8b3a783
Daksh <hello@dak.sh>
2022-01-17 21:53:45 +0400

    fix: 🐛 chainId bug

--- src/constants/networkData.ts
+++ src/constants/networkData.ts
@@ -1,4 +1,4 @@
-export const chainIdOrder = [137, 250, 10, 42161];
+export const chainIdOrder = [137, 250];
 export const networkData: Record<string, any> = {
   137: {
     chainIdHex: "0x89",
@@ -52,5 +52,5 @@ export const networkData: Record<string, any> = {
       sushiURL:
         "https://raw.githubusercontent.com/sushiswap/default-token-list/master/tokens/fantom.json",
     },
-  }
+  },
 };
