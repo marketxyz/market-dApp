5f851d819d391de2605a42b2f6f83769dcd81454
Apoorv <apoorvsingal0@gmail.com>
2022-01-18 13:13:55 +0530

    fix useLiquidationPrice hooks

--- src/hooks/useLiquidationPrice.ts
+++ src/hooks/useLiquidationPrice.ts
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { useBorrowLimit } from "./useBorrowLimit";
+import { useBorrowLimit, useBorrowLimits } from "./useBorrowLimit";
 import { USDPricedFuseAsset } from "utils/fetchFusePoolData";
 
 export const useLiquidationPriceForSuppliedAsset = (
@@ -12,19 +12,18 @@ export const useLiquidationPriceForSuppliedAsset = (
   const borrowLimitForAsset = useBorrowLimit([asset], options);
   const borrowLimitWithoutAsset = borrowLimit - borrowLimitForAsset;
   const difference = borrowBalanceUSD - borrowLimitWithoutAsset;
-  
+
   if (
     difference <= 0 ||
-    options?.ignoreIsEnabledCheckFor === asset.cToken ||
-    asset.membership
+    asset.supplyBalance == 0 ||
+    options?.ignoreIsEnabledCheckFor === asset.cToken
   ) {
-    const totalSupplyNeededUSD = difference / (asset.collateralFactor / 1e18);
-    const liquidationPriceUSD = totalSupplyNeededUSD / asset.supplyBalance;
-
-    return liquidationPriceUSD;
-  } else {
     return 0;
   }
+  const totalSupplyNeededUSD = difference * 1e18 / (asset.collateralFactor);
+  const liquidationPriceUSD = totalSupplyNeededUSD / (asset.supplyBalance / 10 ** asset.underlyingDecimals);
+
+  return liquidationPriceUSD;
 };
 
 export const useLiquidationPricesForSuppliedAssets = (
@@ -33,26 +32,26 @@ export const useLiquidationPricesForSuppliedAssets = (
   options?: { ignoreIsEnabledCheckFor: string }): number[] => {
 
   const borrowLimit = useBorrowLimit(assets, options);
+  const borrowLimits = useBorrowLimits(assets.map(asset => [asset]), options);
 
   const liquidationPricesUSD = useMemo(() => {
-    return assets.map(asset => {
-      const borrowLimitForAsset = useBorrowLimit([asset], options);
+    return assets.map((asset, i) => {
+      const borrowLimitForAsset = borrowLimits![i];
       const borrowLimitWithoutAsset = borrowLimit - borrowLimitForAsset;
-
       const difference = borrowBalanceUSD - borrowLimitWithoutAsset;
       
       if (
         difference <= 0 ||
-        options?.ignoreIsEnabledCheckFor === asset.cToken ||
-        asset.membership
+        asset.supplyBalance == 0 ||
+        options?.ignoreIsEnabledCheckFor === asset.cToken
       ) {
-        const totalSupplyNeededUSD = difference / (asset.collateralFactor / 1e18);
-        const liquidationPriceUSD = totalSupplyNeededUSD / asset.supplyBalance;
-
-        return liquidationPriceUSD;
-      } else {
         return 0;
       }
+      const totalSupplyNeededUSD = difference / (asset.collateralFactor / 1e18);
+      const liquidationPriceUSD = totalSupplyNeededUSD / (asset.supplyBalance / 10 ** asset.underlyingDecimals);
+
+      return liquidationPriceUSD;
+
     });
   }, [assets, borrowBalanceUSD, options?.ignoreIsEnabledCheckFor]);
 
@@ -64,10 +63,14 @@ export const useLiquidationPriceForBorrowedAsset = (
   assets: USDPricedFuseAsset[],
   borrowBalanceUSD: number,
   options?: { ignoreIsEnabledCheckFor: string }): number => {
-
+  
   const borrowLimit = useBorrowLimit(assets, options);
+  
+  if(asset.borrowBalance == 0){
+    return 0;
+  }
   const difference = borrowLimit - borrowBalanceUSD;
-  const liquidationPriceUSD = (asset.borrowBalanceUSD + difference)/asset.borrowBalance;
+  const liquidationPriceUSD = (asset.borrowBalanceUSD + difference) / (asset.borrowBalance / 10 ** asset.underlyingDecimals);
 
   return liquidationPriceUSD;
 };
@@ -81,8 +84,11 @@ export const useLiquidationPricesForBorrowedAssets = (
 
   const liquidationPricesUSD = useMemo(() => {
     return assets.map(asset => {
+      if(asset.borrowBalance == 0){
+        return 0;
+      }
       const difference = borrowLimit - borrowBalanceUSD;
-      const liquidationPriceUSD = (asset.borrowBalanceUSD + difference)/asset.borrowBalance;
+      const liquidationPriceUSD = (asset.borrowBalanceUSD + difference) / (asset.borrowBalance / 10 ** asset.underlyingDecimals);
 
       return liquidationPriceUSD;
     });
